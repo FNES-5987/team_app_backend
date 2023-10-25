fa46d10956a2339b758e7c627a28756e1c6a0d87
Sohyun kim <rlathgus541@naver.com>
2023-10-25 14:41:26 +0900

    DB추가시, 중복 및 오류 관련 로직 추가

--- src/main/kotlin/com/example/app_backend/book/BookController.kt
+++ src/main/kotlin/com/example/app_backend/book/BookController.kt
@@ -1,5 +1,6 @@
 package com.example.app_backend.book
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -16,18 +17,20 @@ class BookController(private val bookService: BookService) {
     // 추가
     @PostMapping("/add")
     fun addBook(@RequestBody book: SimplifiedBookDTO): ResponseEntity<List<SimplifiedBookDTO>> {
-
-        // DB 업데이트 기능 포함
-        val updatedBooks = bookService.addBook(book)
-        println("addBooks 응답 성공")
-        println("${updatedBooks}")
-        return ResponseEntity.ok(updatedBooks)
+        val books = bookService.addBook(book)
+        //        println("AddBooks 응답 성공")
+        return if (books.isNotEmpty()) {
+            ResponseEntity.ok(books)
+        } else {
+            ResponseEntity.status(HttpStatus.CONFLICT).body(emptyList())
+        }
     }
 
     @DeleteMapping("/{bookId}")
     fun deleteBook(@PathVariable bookId: Int): ResponseEntity<List<SimplifiedBookDTO>> {
         // DB 업데이트 기능 포함
         val updatedBooks = bookService.deleteBook(bookId)
+        println("deleteBooks 응답 성공")
         return ResponseEntity.ok(updatedBooks)
     }
 
--- src/main/kotlin/com/example/app_backend/book/BookService.kt
+++ src/main/kotlin/com/example/app_backend/book/BookService.kt
@@ -35,10 +35,12 @@ class BookService(
 //            println("Cacheed data: $cacheedData")
             try {
                 mapper.readValue(cacheData)
+
             } catch (e: JsonParseException) {
                 println("Json parsing error: ${e.message}")
                 emptyList()
             }
+
         } else {
             println("Data from MySQL")
             // DB 가져옴 (호출)
@@ -54,6 +56,7 @@ class BookService(
 //            가져온 책의 리스트를 반환
             return books
         }
+
     }
 
     // 최신 DB정보 cache에 업데이트
@@ -71,9 +74,9 @@ class BookService(
         }
 
         if (newBook != null) {
-            println("삭제 요청 들어 옴")
+            println("추가 요청 들어 옴")
             // 새로운 책이 있다면, 기존 정보에 새로운 책 정보를 추가
-            val isNewBook = existingBooks.none { it.id == newBook.id }
+            val isNewBook = existingBooks.none { it.isbn == newBook.isbn || it.isbn13 == newBook.isbn13 }
             if (isNewBook) {
                 val updatedBooks = existingBooks + newBook
                 redisTemplate.opsForValue().set("books", mapper.writeValueAsString(updatedBooks))
@@ -117,35 +120,37 @@ class BookService(
 
     fun addBook(book: SimplifiedBookDTO): List<SimplifiedBookDTO> {
         println("addBook 요청 들어옴")
-        val currentTime = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm")
-        val formattedDate = currentTime.format(formatter)
-        // DB에 책을 추가
-        val addBook = transaction {
-            SimplifiedBooks.insertAndGetId {
-                it[createdDate] = formattedDate
-                it[title] = book.title
-                it[link] = book.link
-                it[author] = book.author
-                it[pubDate] = book.pubDate
-                it[description] = book.description
-                it[isbn] = book.isbn
-                it[isbn13] = book.isbn13
-                it[itemId] = book.itemId
-                it[priceSales] = book.priceSales
-                it[priceStandard] = book.priceStandard
-                it[stockStatus] = book.stockStatus
-                it[cover] = book.cover
-                it[categoryId] = book.categoryId
-                it[categoryName] = book.categoryName
-                it[publisher] = book.publisher
-                it[customerReviewRank] = book.customerReviewRank
+        val existingBooks = getBooks()
+        val isBook = existingBooks.filter { it.isbn == book.isbn || it.isbn13 == book.isbn13 }
+        if (isBook.isEmpty()) {
+            val currentTime = LocalDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm")
+            val formattedDate = currentTime.format(formatter)
+            // DB에 책을 추가
+            val addBook = transaction {
+                SimplifiedBooks.insertAndGetId {
+                    it[createdDate] = formattedDate
+                    it[title] = book.title
+                    it[link] = book.link
+                    it[author] = book.author
+                    it[pubDate] = book.pubDate
+                    it[description] = book.description
+                    it[isbn] = book.isbn
+                    it[isbn13] = book.isbn13
+                    it[itemId] = book.itemId
+                    it[priceSales] = book.priceSales
+                    it[priceStandard] = book.priceStandard
+                    it[stockStatus] = book.stockStatus
+                    it[cover] = book.cover
+                    it[categoryId] = book.categoryId
+                    it[categoryName] = book.categoryName
+                    it[publisher] = book.publisher
+                    it[customerReviewRank] = book.customerReviewRank
+                }
             }
+            return updateCache(book)
         }
-        println("updateCashe :" +
-                "${updateCache(book)}")
-        // 캐시 업데이트
-        return updateCache(book)
+        return emptyList()
     }
 
     fun deleteBook(bookId: Int): List<SimplifiedBookDTO> {
