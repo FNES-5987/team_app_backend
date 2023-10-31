24ed15769764d1680b931bb81ba7695032efe4be
JBJ <maystar5987@gmail.com>
2023-10-31 13:18:29 +0900

    redis 설정변경

--- src/main/kotlin/com/example/app_backend/redis/RedisService.kt
+++ src/main/kotlin/com/example/app_backend/redis/RedisService.kt
@@ -1,24 +1,21 @@
 package com.example.app_backend.redis
 
 import com.example.app_backend.statistics.RedisData
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.ScanOptions
 import org.springframework.stereotype.Service
-import kotlin.math.min
 
 @Service
 class RedisService(private val redisTemplate: RedisTemplate<String, String>) {
 
-    fun getAllData(page: Int, size: Int): List<RedisData> {
-        val pageRequest = PageRequest.of(page, size)
-        val scanOptions = ScanOptions.scanOptions().match("*").count(pageRequest.pageSize.toLong()).build()
+    fun getAllData(): List<RedisData> {
+        val scanOptions = ScanOptions.scanOptions().match("*").build()
         val cursor = redisTemplate.executeWithStickyConnection { it.scan(scanOptions) }
 
         val redisDataList = mutableListOf<RedisData>()
         cursor.use {
             it.forEachRemaining {
-                val itemId = it.toString()
+                val itemId = String(it, Charsets.UTF_8) // UTF-8 문자열로 변환
                 val stockstatus = try {
                     redisTemplate.opsForValue().get(itemId) ?: ""
                 } catch (e: Exception) {
@@ -29,9 +26,6 @@ class RedisService(private val redisTemplate: RedisTemplate<String, String>) {
             }
         }
 
-        // 페이지에 맞는 데이터를 반환합니다.
-        val start = pageRequest.offset.toInt()
-        val end = min(start + pageRequest.pageSize, redisDataList.size)
-        return redisDataList.subList(start, end)
+        return redisDataList
     }
 }
--- src/main/kotlin/com/example/app_backend/statistics/StatisticsController.kt
+++ src/main/kotlin/com/example/app_backend/statistics/StatisticsController.kt
@@ -1,20 +1,17 @@
 package com.example.app_backend.statistics
 
 import com.example.app_backend.redis.RedisService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
 class StatisticsController(private val redisService: RedisService) {
 
     @GetMapping("/redis-data")
-    fun getRedisData(
-            @RequestParam(value = "page", defaultValue = "0") page: Int,
-            @RequestParam(value = "size", defaultValue = "10") size: Int
-    ): List<RedisData> {
-        return redisService.getAllData(page, size)
+    fun getRedisData(): List<RedisData> {
+        return redisService.getAllData()
     }
+
 }
